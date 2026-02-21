22630c5afd5abc573e8716e3590c46ecee26c92c
LORD PREMO <cypherx599@gmail.com>
2026-02-21 12:59:18 +0300

    Create pair.js

---
+++ api/pair.js
@@ -0,0 +1,14 @@
+{
+  "name": "broken-lord-md-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "latest",
+    "express": "latest",
+    "node-fetch": "latest",
+    "qrcode-terminal": "latest"
+  }
+}
